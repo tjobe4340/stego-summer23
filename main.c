89f9d2a46b7ebfbf6ab76b6b80088a689ed4f37c
tjobe4340 <tjobe4340@gmail.com>
2023-07-10 22:23:52 -0500

    start of function

--- main.c
+++ main.c
@@ -7,3 +7,20 @@ int main(int argc, char* argv[]){
     printf("Hello world\n");
     return 0;
 }
+
+void changeContrast(int change){
+    /* f(x)= ax+b or f(x)= a(x-128)+128+b where a is contrast and b is brightness
+    *
+    * or F=(259(C+255))/(255(259-C)) stored as a float, and C is desired level of contrast
+    * factor = (259 * (contrast + 255)) / (255 * (259 - contrast))
+    * color = GetPixelColor(x, y) //should get color
+    * newRed   = Truncate(factor * (Red(colour)   - 128) + 128) //ensure value is between 0 and 255
+    * newGreen = Truncate(factor * (Green(colour) - 128) + 128) //ensure value is between 0 and 255
+    * newBlue  = Truncate(factor * (Blue(colour)  - 128) + 128) //ensure value is between 0 and 255
+    * PutPixelColor(x, y) = RGB(newRed, newGreen, newBlue)
+    * R'=F(R-128)+128
+    */
+    
+
+    
+}
--- main.h
+++ main.h
@@ -1,3 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
+void changContrast(int change);
